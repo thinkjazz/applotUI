25b6b3c14ea74d7079ee89e97f9a444cb0f86732
Борик <takeacutter@gmail.com>
2019-02-27 17:09:01 +0300

    Calendar

--- assets/js/scripts/calendar-demo.js
+++ assets/js/scripts/calendar-demo.js
@@ -31,47 +31,47 @@ $(document).ready(function() {
             center: 'title',
             right: 'month,basicWeek,basicDay'
         },
-        defaultDate: '2017-10-12',
+        defaultDate: '2019-10-12',
         navLinks: true, // can click day/week names to navigate views
         editable: true,
         droppable: true, // this allows things to be dropped onto the calendar
         eventLimit: true, // allow "more" link when too many events
         events: [
             {
-                title: 'All Day Event',
-                start: '2017-10-01',
+                title: 'Событие',
+                start: '2019-10-01',
                 backgroundColor: '#e74c3c'
             },
             {
-                title: 'Long Event',
-                start: '2017-10-07',
-                end: '2017-10-10',
+                title: 'Долгая лотерия',
+                start: '2019-10-07',
+                end: '2019-10-10',
                 backgroundColor: '#2ecc71'
             },
             {
-                title: 'Conference',
-                start: '2017-10-11',
-                end: '2017-10-13',
+                title: 'Платная лотерея',
+                start: '2019-10-11',
+                end: '2019-10-13',
                 backgroundColor: '#23B7E5'
             },
             {
-                title: 'Meeting',
-                start: '2017-10-12T10:30:00',
-                end: '2017-10-12T12:30:00'
+                title: 'Розыгрыш пепяк',
+                start: '2019-10-12',
+                end: '2019-10-13'
             },
             {
-                title: 'Dinner',
-                start: '2017-10-12T20:00:00'
+                title: 'Тупая макака',
+                start: '2019-10-12T'
             },
             {
-                title: 'Birthday Party',
-                start: '2017-10-13T07:00:00',
-                backgroundColor: '#F39C12'
+                title: 'Purple Haze',
+                start: '2019-10-13',
+                backgroundColor: '#546de5'
             },
             {
-                title: 'Click for Google',
+                title: 'Кликни Google',
                 url: 'http://google.com/',
-                start: '2017-10-28'
+                start: '2019-10-28'
             }
         ],
         drop: function() {
